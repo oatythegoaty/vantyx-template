fe078e781b8f5b77722e84ee9dd20fad96e151e3
Ty Howard <tyseanhoward@Tys-MacBook-Air.attlocal.net>
2025-05-24 23:09:38 -0400

    Add _app.tsx to enable Tailwind global styles

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+
+module.exports = {
+  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+      },
+      colors: {
+        magenta: '#CA3CFF',
+        blue: '#2C3EFA',
+      },
+    },
+  },
+  plugins: [],
+};
