7e76ad96a3234a020399291ebcd62ce00bd3b0c4
Ty Howard <tyseanhoward@Tys-MacBook-Air.attlocal.net>
2025-05-25 00:32:03 -0400

    Fix Tailwind content config and animations

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,9 +2,7 @@ import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './app/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{ts,tsx}',
   ],
   theme: {
     extend: {
